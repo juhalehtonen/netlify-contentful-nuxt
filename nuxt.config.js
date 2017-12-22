c64d071f9cec27de6b977a1b5b5b8a56f29523cf
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2017-12-22 15:19:05 +0200

    Update metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt + Contentful = <3' }
+      { hid: 'description', name: 'description', content: 'Nuxt + Contentful @ Netlify' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
